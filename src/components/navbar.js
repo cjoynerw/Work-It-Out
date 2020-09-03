ca59c5eb690094c84be62159180e031a4c06bde0
cjoynerw <cjoynerw@gmail.com>
2020-09-03 13:17:11 -0500

    created user auth, added buttons that log in and logout user

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LoginButton from './LoginButton'
+import LogoutButton from './LogoutButton';
 
 export default class Navbar extends Component {
 
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Fit Boi</Link>
+        <Link to="/" className="navbar-brand">Work It Out Planner</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
           <Link to="/" className="nav-link">Exercises</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <Link to="/create" className="nav-link">Create Exercise</Link>
           </li>
           <li className="navbar-item">
           <Link to="/user" className="nav-link">Create User</Link>
           </li>
+          <LoginButton />
+          <LogoutButton />
         </ul>
         </div>
       </nav>
